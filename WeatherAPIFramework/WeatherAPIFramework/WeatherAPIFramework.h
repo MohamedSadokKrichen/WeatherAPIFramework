508b27c4295c1208c1a009d0837da557e3890689
Sadok Krichen <sadok.krichen@addinn.com>
2023-03-07 12:47:27 +0100

    Premier commit - creation du projet

---
+++ WeatherAPIFramework/WeatherAPIFramework/WeatherAPIFramework.h
@@ -0,0 +1,18 @@
+//
+//  WeatherAPIFramework.h
+//  WeatherAPIFramework
+//
+//  Created by Mohamed Sadok  Krichen on 7/3/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherAPIFramework.
+FOUNDATION_EXPORT double WeatherAPIFrameworkVersionNumber;
+
+//! Project version string for WeatherAPIFramework.
+FOUNDATION_EXPORT const unsigned char WeatherAPIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherAPIFramework/PublicHeader.h>
+
+
